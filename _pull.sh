52531d9603cfa038a0ffa071dbacd737cdd9f2ba
Eric Torre González <etorre@plainconcepts.com>
2020-03-01 13:44:47 +0100

    add elastic search and kibana

--- _pull.sh
+++ _pull.sh
@@ -1,16 +1,20 @@
 DIRECTORIES=""
+DIRECTORIES="${DIRECTORIES} ."
 DIRECTORIES="${DIRECTORIES} ./Src/Apis/products/"
 DIRECTORIES="${DIRECTORIES} ./Src/Apis/stock/"
 DIRECTORIES="${DIRECTORIES} ./Src/Apis/discounts/"
 DIRECTORIES="${DIRECTORIES} ./Src/Services/identityServer/"
 DIRECTORIES="${DIRECTORIES} ./Src/Clients/WebStatus/"
 DIRECTORIES="${DIRECTORIES} ./Src/Clients/lanreshop/"
+DIRECTORIES="${DIRECTORIES} ./Src/Shared/Lanre.Shared.Api/"
 
 echo '-- List of directories'
 
 for DIRECTORY in ${DIRECTORIES}; do
     echo "    ${DIRECTORY}"
     git -C ${DIRECTORY} pull
+    # auth mode
+    # git clone https://user:pass@repo
 done
 
 echo '-- Finished'
